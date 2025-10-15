d917c0489fa49dc27e02db40b9ab609afa036819
mjngxwnj <mjngxujian289@gmail.com>
2025-10-15 20:45:12 +0700

    feat(terraform): update S3 module

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,14 +1,27 @@
-provider "aws" {
-  region = var.region
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  tags   = var.tags
 }
 
-resource "aws_s3_bucket" "huynhthuan_bucket" {
-  bucket = var.bucket_name
-  acl    = var.acl
+#resource "aws_s3_bucket_acl" "this" {
+#  bucket = aws_s3_bucket.this.id
+#  acl    = var.acl
+#}
 
-  versioning {
-    enabled = var.versioning
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = var.versioning ? "Enabled" : "Suspended"
   }
+}
 
-  tags = var.tags
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
--- terraform/modules/s3/variables.tf
+++ terraform/modules/s3/variables.tf
@@ -1,24 +1,24 @@
-variable "region" {
-  type    = string
-  default = "ap-southeast-1"
-}
-
+#Require root module provide
 variable "bucket_name" {
+  description = "Name of the S3 bucket for data storage"
   type    = string
-  default = "huynhthuan-bucket"
 }
 
+variable "tags" {
+  description = "A map of tags to assign to the bucket"
+  type    = map(string)
+}
+
+
+#Optional, has default value
 variable "acl" {
+  description = "Access control list for the bucket"
   type    = string
   default = "private"
 }
 
 variable "versioning" {
+  description = "Enable versioning for the bucket"
   type    = bool
   default = true
-}
-
-variable "tags" {
-  type    = map(string)
-  default = {}
 }
